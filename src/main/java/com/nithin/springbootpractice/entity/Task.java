f33712257eecacd9c173dd7cf1f1ebede5964bdb
Nithin <nknithinabc@gmail.com>
2023-12-27 13:20:39 +0530

    Updated TaskEntity

--- src/main/java/com/nithin/springbootpractice/entity/Task.java
+++ src/main/java/com/nithin/springbootpractice/entity/Task.java
@@ -2,9 +2,24 @@ package com.nithin.springbootpractice.entity;
 
 import lombok.Data;
 
-@Data
 public class Task {
-    private String taskId;
-    private String taskName;
-}
+	private String taskId;
+	private String taskName;
 
+	public String getTaskId() {
+		return taskId;
+	}
+
+	public void setTaskId(String taskId) {
+		this.taskId = taskId;
+	}
+
+	public String getTaskName() {
+		return taskName;
+	}
+
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+
+}
