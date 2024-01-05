a987cff44ce72eb15204ce0ba104afd2154ae6f6
Nithin <nknithinabc@gmail.com>
2024-01-05 14:08:02 +0530

    Read input from File

--- src/main/java/com/nithin/springbootpractice/service/ReadFromFileUsingScanner.java
+++ src/main/java/com/nithin/springbootpractice/service/ReadFromFileUsingScanner.java
@@ -15,5 +15,23 @@ public class ReadFromFileUsingScanner {
         	StringTokenizer st = new StringTokenizer(line," ");
         	System.out.println(st.countTokens());
         }
+        
+        String input1 = "Hello  World";
+        String[] words1 = input1.split("\\s");
+        
+        System.out.println("Words separated by single whitespace:");
+        for (String word : words1) {
+            System.out.println(word);
+        }
+
+        // Example for \\s+ (one or more whitespace characters)
+        String input2 = "   Java   Regular   Expressions   ";
+        String[] words2 = input2.split("\\s+");
+
+        System.out.println("\nWords separated by one or more whitespaces:");
+        for (String word : words2) {
+            System.out.println(word);
+        }
+        
     }
 }
