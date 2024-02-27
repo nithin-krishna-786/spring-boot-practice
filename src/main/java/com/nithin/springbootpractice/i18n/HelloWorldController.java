3f56ff8b55242bb6ef3323e8182002b3fa56de3e
Nithin <nknithinabc@gmail.com>
2024-02-27 21:41:21 +0530

    i18n

---
+++ src/main/java/com/nithin/springbootpractice/i18n/HelloWorldController.java
@@ -0,0 +1,35 @@
+package com.nithin.springbootpractice.i18n;
+
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	private MessageSource messageSource;
+	
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+	
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale );
+		
+		//return "Hello World V2"; 
+		
+		//1:
+		//2:
+//		- Example: `en` - English (Good Morning)
+//		- Example: `nl` - Dutch (Goedemorgen)
+//		- Example: `fr` - French (Bonjour)
+//		- Example: `de` - Deutsch (Guten Morgen)
+
+	}
+
+}
