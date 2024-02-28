c2e0073d854ced9dcd743c316749c9e3026d07c9
Nithin <nknithinabc@gmail.com>
2024-02-28 18:56:49 +0530

    API Versioning

---
+++ src/main/java/com/nithin/springbootpractice/versioning/PersonV1.java
@@ -0,0 +1,20 @@
+package com.nithin.springbootpractice.versioning;
+
+public class PersonV1 {
+	private String name;
+
+	public PersonV1(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return "PersonV1 [name=" + name + "]";
+	}
+
+}
---
+++ src/main/java/com/nithin/springbootpractice/versioning/PersonV2.java
@@ -0,0 +1,20 @@
+package com.nithin.springbootpractice.versioning;
+
+public class PersonV2 {
+	private Name name;
+
+	public PersonV2(Name name) {
+		super();
+		this.name = name;
+	}
+
+	public Name getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return "PersonV2 [name=" + name + "]";
+	}
+
+}
