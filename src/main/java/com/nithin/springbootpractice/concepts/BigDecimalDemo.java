6e1a3b17c2e87cddfb073d45146add433bcbe33e
Nithin <nknithinabc@gmail.com>
2024-03-26 19:11:11 +0530

    Big Decimal Demo

---
+++ src/main/java/com/nithin/springbootpractice/concepts/BigDecimalDemo.java
@@ -0,0 +1,34 @@
+package com.nithin.springbootpractice.concepts;
+
+import java.math.BigDecimal;
+
+public class BigDecimalDemo {
+
+	public static void main(String[] args) {
+
+	      // Creating BigDecimal objects with specified values
+        BigDecimal num1 = new BigDecimal("0.0000000000000000000001");
+        BigDecimal num2 = new BigDecimal("0.9999999999999999999999");
+
+        // Performing arithmetic operations with high precision
+        BigDecimal sum = num1.add(num2); // Addition
+        BigDecimal difference = num2.subtract(num1); // Subtraction
+        BigDecimal product = num1.multiply(num2); // Multiplication
+        
+        // Displaying the results
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        
+        String fl1 = "0.002";
+        String fl2 = "0.003";
+        
+        BigDecimal num3 = new BigDecimal(fl1);
+        BigDecimal num4 = new BigDecimal(fl2);
+        BigDecimal sum2 = num3.add(num4); // Addition
+        System.out.println("Sum: " + sum2);
+
+        
+	}
+
+}
