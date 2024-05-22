465bd6295a65d2975aff0cd37c80edbe63b4bcc5
Nithin <nknithinabc@gmail.com>
2024-05-22 10:45:24 +0530

    Twillio API

--- src/main/java/com/nithin/springbootpractice/twillio/sms/SMSController.java
+++ src/main/java/com/nithin/springbootpractice/twillio/sms/SMSController.java
@@ -1,5 +1,6 @@
 package com.nithin.springbootpractice.twillio.sms;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,18 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 
+import jakarta.validation.Valid;
+
 
 @RestController
 @RequestMapping("/api/sms")
 public class SMSController {
 	
-	private String ACCOUNT_SID="ACCOUNT_SID";
-	private String AUTH_TOKEN="AUTH_TOKEN";
+	
+	@Value("twillio.account-sid")
+	private String accountSid;
+	
+	@Value("twillio.account.secret-key")
+	private String authToken;
 	
 	@PostMapping("/send")
 	public ResponseEntity<SMSMessage>sendSMS(@RequestBody SMSMessage smsReq){
 		
-        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        Twilio.init(accountSid, authToken);
         Message message = Message.creator(
                 new com.twilio.type.PhoneNumber(smsReq.getMobile()),
                 new com.twilio.type.PhoneNumber("+91 7893913827"),
